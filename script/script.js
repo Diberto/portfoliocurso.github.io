3c229b7cab203497e41dcbebad126670d30d5d32
Diberto <srlomax@hotmail.com>
2023-03-07 23:17:22 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// agrega las clase navbarDark en navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapsa navbar despues de click en mobile
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
